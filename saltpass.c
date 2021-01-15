b2bd994784308a62482d2aad797c7a57ca6a4f19
zaid gharaybeh <z.gharaybeh@gmail.com>
2021-01-15 13:59:19 -0800

    format

--- saltpass.c
+++ saltpass.c
@@ -67,7 +67,7 @@ int main(int argc, char* argv[])
     //read string that includes salt (e.g. website), user, master password, state (e.g. iteration)
     char full_string[BUFSIZ]; 
     memclear_string(full_string, sizeof(full_string));
-    puts("Insert full string e.g. '<salt><user-initial><password>[U[P]][state]':");
+    puts("Insert full string e.g. '<salt><user-initial><password>[U[state][P]][state]':");
     read_string(full_string, terminal_echo);
 
     // pass to sha256 hash function
